8fdc9641bd295c2bcc8b78223e64d2abe72311bf
Rafael Norte <rnrfnorte@hotmail.com>
2024-05-28 13:02:42 +0100

    init

---
+++ database/migrations/2024_05_28_113952_create_questions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+
+            Schema::create('questions', function (Blueprint $table) {
+                $table->increments('question_id');
+                $table->integer('exam_id');
+                $table->string('name', 1024);
+                $table->integer('score');
+
+                // Foreign key constraint
+                $table->foreign('exam_id')->references('exam_id')->on('exams')->onDelete('cascade');
+            });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('questions');
+    }
+};
