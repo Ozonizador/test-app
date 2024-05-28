00d2ad1a37131a887065324f7c2afaffede90412
Rafael Norte <rnrfnorte@hotmail.com>
2024-05-28 13:39:07 +0100

    fix on creating fk in questions

--- database/migrations/2024_05_28_113952_create_questions_table.php
+++ database/migrations/2024_05_28_113952_create_questions_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
 
             Schema::create('questions', function (Blueprint $table) {
                 $table->increments('question_id');
-                $table->integer('exam_id');
+                $table->unsignedInteger('exam_id');
                 $table->string('name', 1024);
                 $table->integer('score');
 
