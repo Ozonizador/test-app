40be854e4cf6ed37fab044c27becc3c5388c2dfd
Rafael Norte <rnrfnorte@hotmail.com>
2024-06-06 00:44:26 +0100

    Fix on var name

--- resources/views/exams/exam-result.blade.php
+++ resources/views/exams/exam-result.blade.php
@@ -10,29 +10,39 @@
             <div class="row justify-content-md-center">
                 <div class="col-md-9 ">
                     <div class="card px-5 py-3 mt-5 shadow">
-                        @if ($total_score > $exam->required_score)
-                            <h1 class="text-center mt-3 mb-4">Congratulations
-                                @if (Auth::check())
-                                    {{ Auth::user()->name }}
-                                @else
-                                    {{ $participant }}
-                                @endif
-                                , You have passed!
-                            </h1>
-                            <h4>You have scored {{ $total_score }} points ({{ $total_score / $maxScore }}) in the
-                                {{ $exam->name }}</h4>
-                        @else
-                            <h1 class="text-center mt-3 mb-4">
-                                @if (Auth::check())
-                                    {{ Auth::user()->name }}
-                                @else
-                                    {{ $participant }}
-                                @endif
-                                , You have failed.
-                            </h1>
-                            <h4>You have scored {{ $total_score }} points ({{ $total_score / $maxScore }}) in the
-                                {{ $exam->name }}</h4>
+                        @if ($max_score != 0)
+                            @if ($total_score > $exam->required_score)
+                                <h1 class="text-center mt-3 mb-8">Congratulations
+                                    @if (Auth::check())
+                                        {{ Auth::user()->name }}
+                                    @else
+                                        {{ $participant }}
+                                    @endif
+                                    , You have passed!
+                                </h1>
+                                <h4 class="mb-8">You have scored {{ $total_score }} points
+                                    ({{ $total_score / $max_score }}%) in the
+                                    {{ $exam->name }} exam.</h4>
+                            @else
+                                <h1 class="text-center mt-3 mb-8">
+                                    @if (Auth::check())
+                                        {{ Auth::user()->name }}
+                                    @else
+                                        {{ $participant }}
+                                    @endif
+                                    , You have failed.
+                                </h1>
+                                <h4 class="mb-8">You have scored {{ $total_score }} points
+                                    ({{ $total_score / $max_score }}%) in the
+                                    {{ $exam->name }} exam.</h4>
+                            @endif
                         @endif
+
+                        <div class="full-width">
+                            <a class="float-right button" href="{{ route('exam.choose.get') }}">
+                                <button type="button">Take another exam</button>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
